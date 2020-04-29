806a8551c92fe24085b69bb7fa061943099b98cc
yang_going <2210744381@qq.com>
2020-04-29 13:02:06 +0800

    导航菜单实现

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -5,11 +5,25 @@ import {
     Form,
     FormItem,
     Input,
-    Message
+    Message,
+    Header,
+    Container,
+    Main,
+    Aside,
+    Menu,
+    Submenu,
+    MenuItem
 } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
 Vue.prototype.$message = Message

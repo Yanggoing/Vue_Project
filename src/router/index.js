8a3638adffe819c3f6fb71619c8a88f0894bc5e7
yang_going <2210744381@qq.com>
2020-05-04 12:25:35 +0800

    分类功能

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Welcome from '../components/Welcome.vue'
 import Users from '../components/user/Users.vue'
 import Rights from '../components/power/Rights.vue'
 import Roles from '../components/power/Roles.vue'
+import Cate from '../components/goods/Cate.vue'
 
 Vue.use(VueRouter)
 
@@ -28,11 +29,14 @@ const routes = [{
   }, {
     path: '/rights',
     component: Rights
-  },{
+  }, {
     path: '/roles',
     component: Roles
-  },]
-},]
+  }, {
+    path: '/categories',
+    component: Cate
+  }, ]
+}, ]
 
 const router = new VueRouter({
   routes
